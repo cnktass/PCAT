1e19631693a16d14b01ca6c927d933f6f56c2eea
cnktass <cnktass@gmail.com>
2024-08-14 14:54:49 +0300

    Gorsel Yukleme,guncelleme,silme Islemleri

--- app.js
+++ app.js
@@ -1,14 +1,103 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
+const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
+const Photo = require('./models/Photo');
+
 const app = express();
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db')
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(fileUpload());
+app.use(methodOverride('_method',{
+  methods:['POST','GET']
+}));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
+});
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda baslatildi...`);
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  const uploadDir = path.join(__dirname, 'public', 'uploads');
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = path.join(uploadDir, uploadeImage.name);
+
+  console.log('Uploading to:', uploadPath);
+
+  uploadeImage.mv(uploadPath, async (err) => {
+    if (err) {
+      console.error('File upload failed:', err);
+      return res.status(500).send(err);
+    }
+
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
+});
+
+app.get('/photos/edit/:id', async (req, res) => {
+  try {
+    const photo = await Photo.findOne({ _id: req.params.id });
+    if (!photo) {
+      return res.status(404).send('Photo not found');
+    }
+    res.render('edit', { photo });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server error');
+  }
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+
+  res.redirect(`/photos/${req.params.id}`)
+});
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + '/public' + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndDelete(req.params.id);
+  res.redirect('/');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on http://localhost:3000');
 });
